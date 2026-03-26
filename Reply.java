fe267d8517ee321426c745096331108c0e0f5d19
Kaashi T <133036513+KaashiRaju@users.noreply.github.com>
2026-03-26 20:16:54 +0530

    Create Reply.java

---
+++ Reply.java
@@ -0,0 +1,51 @@
+package com.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+@Entity
+public class Reply {
+
+    @Id
+    private int replyId;
+    private String user;
+    private String replyContent;
+    private String replyType;
+    private String visibility;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    public Reply() {}
+
+    public Reply(int replyId, String user, String replyContent,
+                 String replyType, String visibility, Article article) {
+        this.replyId = replyId;
+        this.user = user;
+        this.replyContent = replyContent;
+        this.replyType = replyType;
+        this.visibility = visibility;
+        this.article = article;
+    }
+
+    public int getReplyId() { return replyId; }
+    public void setReplyId(int replyId) { this.replyId = replyId; }
+
+    public String getUser() { return user; }
+    public void setUser(String user) { this.user = user; }
+
+    public String getReplyContent() { return replyContent; }
+    public void setReplyContent(String replyContent) { this.replyContent = replyContent; }
+
+    public String getReplyType() { return replyType; }
+    public void setReplyType(String replyType) { this.replyType = replyType; }
+
+    public String getVisibility() { return visibility; }
+    public void setVisibility(String visibility) { this.visibility = visibility; }
+
+    public Article getArticle() { return article; }
+    public void setArticle(Article article) { this.article = article; }
+}
